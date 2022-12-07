185529bbdf2f8356e13d2d6c89bdb97adaf54736
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-12-07 14:44:10 +0300

    Use the search to access an article searched

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,21 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
 
-  # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.search(params[:search])
   end
 
-  # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
 
@@ -34,7 +30,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -47,7 +42,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
 
@@ -58,13 +52,11 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :search)
     end
 end
