cb9c764c5a9ab4ef00db1179ee3fff5874b91437
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-12-08 19:04:46 +0300

    Remove router for counter_articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles do
-    resources :counter_articles, only: %i[ create ]
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "counter_articles/search", to: "articles#search"
-
+  resources :articles
+  
   root "articles#index"
 end
